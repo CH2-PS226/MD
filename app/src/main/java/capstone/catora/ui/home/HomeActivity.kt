656ed18c57a71ed5fc9ca64bdf4a0de8e59d78ac
ilhamfauzan630 <101925259+ilhamfauzan630@users.noreply.github.com>
2023-12-05 16:44:28 +0700

    add home, search, bottom bar

--- app/src/main/java/capstone/catora/ui/home/HomeActivity.kt
+++ app/src/main/java/capstone/catora/ui/home/HomeActivity.kt
@@ -3,13 +3,18 @@ package capstone.catora.ui.home
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import capstone.catora.R
+import capstone.catora.data.ArtWorkProfile
 import capstone.catora.databinding.ActivityHomeBinding
+import capstone.catora.ui.profile.ListArtWorkProfileAdapter
 import capstone.catora.ui.profile.ProfileActivity
 import capstone.catora.ui.upload.UploadActivity
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding : ActivityHomeBinding
+    private val list = ArrayList<ArtWorkProfile>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -17,16 +22,49 @@ class HomeActivity : AppCompatActivity() {
 
         supportActionBar?.hide() //this line for remove action bar
 
-        binding.btnToUploadTest.setOnClickListener {
-            startActivity(Intent(this, UploadActivity::class.java))
-        }
-        binding.btnToProfileTest.setOnClickListener {
-            startActivity(Intent(this, ProfileActivity::class.java))
-        }
 
+        binding.rvArtwork.setHasFixedSize(true)
+        binding.rvArtwork.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
 
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_1))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_9))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_1))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_1))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_1))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_11))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_1))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_1))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_2))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_10))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_3))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_9))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_9))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_10))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_11))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_4))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_11))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_4))
+        list.add(ArtWorkProfile(R.drawable.dummy_artwork_10))
+
+        binding.rvArtwork.adapter = ListArtWorkProfileAdapter(list)
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    true
+                }
+                R.id.upload -> {
+                    startActivity(Intent(this, UploadActivity::class.java))
+                    true
+                }
+
+                R.id.profile -> {
+                    startActivity(Intent(this, ProfileActivity::class.java))
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
-
-
-
 }
