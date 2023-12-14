8d90fa30fe4c764433d846f74aefca9438b967c0
BimaJatiKusuma <bimajatifzth03@gmail.com>
2023-12-14 22:45:38 +0700

    login post api

--- app/src/main/java/capstone/catora/ui/login/LoginActivity.kt
+++ app/src/main/java/capstone/catora/ui/login/LoginActivity.kt
@@ -4,13 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
 import capstone.catora.databinding.ActivityLoginBinding
+import capstone.catora.ui.ViewModelFactory
 import capstone.catora.ui.main.MainActivity
 import capstone.catora.ui.register.RegisterActivity
+import capstone.catora.ui.register.RegisterViewModel
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
 
+    private val viewModel by viewModels<LoginViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -18,19 +24,38 @@ class LoginActivity : AppCompatActivity() {
 
         supportActionBar?.hide() //this line for remove action bar
 
-        binding.tvToRegister.setOnClickListener {
-            startActivity(Intent(this, RegisterActivity::class.java))
-            //handling the login logic
+
+        setupAction()
+
+        viewModel.errorLiveData.observe(this){errorMessage ->
+            binding.tvErrorMessage.text = errorMessage
+            binding.tvErrorMessage.visibility = View.VISIBLE
+            showLoading(false)
         }
 
-        //this button only for checking te registration, i would suggest to delete it and change to text "don't have an account? register"
-        binding.btnLogin.setOnClickListener {
+        viewModel.loadingLiveData.observe(this){
+            showLoading(it)
+        }
+
+        viewModel.successLiveData.observe(this){
             startActivity(Intent(this, MainActivity::class.java))
         }
 
     }
 
+    private fun setupAction() {
+        binding.tvToRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
 
+        binding.btnLogin.setOnClickListener {
+            val password = binding.edUsername.text.toString()
+            val name = binding.edPassword.text.toString()
+
+            viewModel.userLogin(name, password)
+
+        }
+    }
 
     private fun showLoading(isLoading: Boolean) {
         if (isLoading) {
